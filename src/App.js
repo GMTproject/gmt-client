6041e91200f75eb2a18b7561dc19b1acb03bf332
kkh061101 <kkh061101@naver.com>
2023-04-04 19:20:47 +0900

    네비게이션 바 이동 구현 시작

--- src/App.js
+++ src/App.js
@@ -7,8 +7,17 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="*" element={<Navigate to={'/map'} replace />} />
-          <Route path="/map" element={<><Nav /><Map /></>} />
+          <Route path="*" element={<Navigate to={'/map/center/1'} replace />} />
+          <Route path="/map" element={<Navigate to={'/map/center/1'} replace />} />
+          <Route path="/map/center" element={<Navigate to={'/map/center/1'} replace />} />
+          <Route path="/map/auditorium" element={<Navigate to={'/map/auditorium/1'} replace />} />
+          <Route path="/map/domitory" element={<Navigate to={'/map/domitory/1'} replace />} />
+          <Route path="/map/center/1" element={<><Nav /><MapCenterFir /></>}></Route>
+          <Route path="/map/center/2" element={<><Nav /><MapCenterSec /></>}></Route>
+          <Route path="/map/center/3" element={<><Nav /><MapCenterThr /></>}></Route>
+          <Route path="/map/center/4" element={<><Nav /><MapCenterFou /></>}></Route>
+          <Route path="/map/auditorium/1" element={<><Nav /><MapAuditorium /></>}></Route>
+          <Route path="/map/domitory/1" element={<><Nav /><MapDomitori /></>}></Route>
           <Route path="/teach" element={<><Nav /><Teach /></>} />
           <Route path="/login" element={<><Login /></>} />
         </Routes>
@@ -24,14 +33,14 @@ function Nav(props) {
   const [mapHov, setMapHov] = useState(false);
   return <div className="navigator">
     <div className="m-text">
-      <div onMouseOver={e => setMapHov(true)} onMouseLeave={e => { setMapHov(false) }}><a href={"/home"}>우리학교 지도</a></div>&nbsp;&nbsp;
+      <div onMouseOver={e => setMapHov(true)} onMouseLeave={e => { setMapHov(false) }}><a href={"/map"}>우리학교 지도</a></div>&nbsp;&nbsp;
       <div><a href={"/teach"} >우리학교 교사</a></div>
     </div>
     {
       mapHov && <div className="mapHov" onMouseOver={e => setMapHov(true)} onMouseLeave={e => setMapHov(false)}>
-        <a href="/"><div>본관</div></a><hr />
-        <a href="/"><div>금봉관</div></a><hr />
-        <a href="/"><div>기숙사</div></a>
+        <a href="/map/center"><div>본관</div></a><hr />
+        <a href="/map/auditorium"><div>금봉관</div></a><hr />
+        <a href="/map/domitory"><div>기숙사</div></a>
       </div>
     }
     <div className="right">
@@ -39,15 +48,36 @@ function Nav(props) {
         <a href='/login'><button>로그인</button></a>&nbsp;
         <button onClick={e => setLogined(e => !e)}>회원가입</button></>
         : <>
-          <div className="clientInfo"></div>
+          <div className="clientInfo">
+            <span>홍길동</span>&nbsp;&nbsp;
+            <button onClick={e => setLogined(e => !e)}>로그아웃</button>
+          </div>
         </>
       }</div>
   </div >;
 }
 
-function Map() {
-  return <div className="map">
-    map
+function MapCenterFir() {
+  return <div className="mapCenter">
+    Center First Floor
+  </div>;
+}
+
+function MapCenterSec() {
+  return <div className="mapCenter">
+    Center Second Floor
+  </div>;
+}
+
+function MapCenterThr() {
+  return <div className="mapCenter">
+    Center Third Floor
+  </div>;
+}
+
+function MapCenterFou() {
+  return <div className="mapCenter">
+    Center Fourth Floor
   </div>;
 }
 
@@ -60,5 +90,17 @@ function Teach() {
 function Login() {
   return <div className="login">
     login
+  </div>;
+}
+
+function MapDomitori() {
+  return <div className="mapDomitori">
+    Domitory
+  </div>;
+}
+
+function MapAuditorium() {
+  return <div className="mapAuditorium">
+    Auditorium
   </div>;
 }
