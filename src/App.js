e773b38388a896e6439e80c262823f43233417da
kkh061101 <kkh061101@naver.com>
2023-04-11 23:41:04 +0900

    설명을 위한 주석 추가

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
+import './App.scss';
+// 이미지 불러오기
 import center1 from './imgs/본관1층.png';
 import goldencrown1 from './imgs/금봉관1층.png';
 import domitory1 from './imgs/기숙사1층.png';
-import './App.scss';
 
-function App() {
+function App() { // 페이지로 보여주는 부분 반드시 필요함. 임의로 삭제하거나 수정하지 말 것.
   const [structure, setStructure] = useState("center"); // center, goldencrown, domitory 3속성
   const [floor, setFloor] = useState(1);
   useEffect(e => {
-    if (localStorage.getItem('structure')) { // 만약 로컬스토리지에 "이전으로 부터 넘겨받은 값"이 있을 경우 그 값으로 대체.
+    if (localStorage.getItem('structure')) { // 만약 로컬스토리지에 "이전에 저장한 값"이 있을 경우 그 값으로 대체.
       setStructure(localStorage.getItem('structure'));
     }
   }, []);
@@ -18,7 +19,10 @@ function App() {
       <BrowserRouter> {/* 페이지 이동하는 부분 가급적 건드리지 마시오. */}
         <Routes>
           <Route path="*" element={<Navigate to={'/map'} replace />} />
-          <Route path="/map" element={<><Nav setStructure={setStructure} /><Map structure={structure} floor={floor} setFloor={setFloor} setStructure={setStructure} /></>} />
+          <Route path="/map" element={<>
+            <Nav setStructure={setStructure} />
+            <Map structure={structure} floor={floor} setFloor={setFloor} setStructure={setStructure} />
+          </>} />
           <Route path="/teach" element={<><Nav setStructure={setStructure} /><Teach /></>} />
           <Route path="/login" element={<><Login /></>} />
         </Routes>
@@ -39,27 +43,31 @@ function Nav({ setStructure }) { // 네비게이션 바
     }
   }, []);
 
-  function setStorage(structure) { // 로컬스토리지에 저장하고 페이지 이동하는 방식
+  function setHere(structure) { // 로컬스토리지에 저장하고 페이지 이동하는 함수
     setStructure(e => structure);
     localStorage.setItem('structure', structure);
   }
 
   return <div className="navigator">
-    <div className="m-text">
-      <div onMouseOver={e => setMapHov(true)} onMouseLeave={e => { setMapHov(false) }}><a href={"/map"}>우리학교 지도</a></div>&nbsp;&nbsp;
-      <div><a href={"/teach"} >우리학교 교사</a></div>
+    <div className="middle-text">
+      {/* 페이지 이동이 가능하게 하는 버튼 */}
+      <div onMouseOver={e => setMapHov(true)} onMouseLeave={e => { setMapHov(false) }}>
+        <a href={"/map"}>학교 지도</a>
+      </div>
+      <div><a href={"/teach"} >학교 교사</a></div>
     </div>
     {
       mapHov && <div className="mapHov" onMouseOver={e => setMapHov(true)} onMouseLeave={e => setMapHov(false)}>
-        <a href="/map" onClick={e => setStorage('center')}><div>본관</div></a><hr />
-        <a href="/map" onClick={e => setStorage('goldencrown')}><div>금봉관</div></a><hr />
-        <a href="/map" onClick={e => setStorage('domitory')}><div>기숙사</div></a>
+        {/* 로컬스토리지에 위치 값을 저장하는 버튼 */}
+        <a href="/map" onClick={e => setHere('center')}><div>본관</div></a><hr />
+        <a href="/map" onClick={e => setHere('goldencrown')}><div>금봉관</div></a><hr />
+        <a href="/map" onClick={e => setHere('domitory')}><div>기숙사</div></a>
       </div>
     }
     <div className="right"> {/* 오른쪽 로그인 버튼 부분 모음 */}
       {!logined ? <>
-        <a href='/login'><button>로그인</button></a>&nbsp;
-        <button onClick={e => { // 로그인하는 부분
+        <a href='/login'><button>로그인</button></a>&nbsp; { /* 로그인 페이지 이동하는 버튼 */}
+        <button onClick={e => { // 회원가입하는 버튼 (임시) 추후 페이지 이동
           setLogined(e => '홍길동');
           localStorage.setItem('logininfo', '홍길동');
           localStorage.setItem('logintime', time.toLocaleTimeString());
@@ -67,7 +75,7 @@ function Nav({ setStructure }) { // 네비게이션 바
         : <>
           <div className="clientInfo">
             <span>{logined}</span>&nbsp;&nbsp;
-            <button onClick={e => { // 로그아웃하는 부분
+            <button onClick={e => { // 로그아웃하는 버튼
               setLogined(e => false);
               localStorage.clear(); // 로그아웃 시 저장된 모든 정보 삭제
             }}>로그아웃</button>
@@ -79,9 +87,10 @@ function Nav({ setStructure }) { // 네비게이션 바
 
 function Map({ structure, floor, setStructure, setFloor }) {
   return <div className="map">
-    <input onChange={e => setFloor(_ => parseInt(e.target.value))} value={floor} />
-    <input onChange={e => setStructure(_ => e.target.value)} value={structure} />
+    &nbsp;<input onChange={e => setFloor(_ => parseInt(e.target.value))} value={floor} />
+    &nbsp;<input onChange={e => setStructure(_ => e.target.value)} value={structure} />
     <div className="img">
+      {/* 지도를 보여주는 부분. 앞 조건이 모두 만족을 하게 되면 뒤에 있는 이미지 태그가 보이게 함 */}
       {structure === 'center' && floor === 1 && <img src={center1} alt={'center1'} />}
       {structure === 'goldencrown' && floor === 1 && <img src={goldencrown1} alt={'goldencrown1'} />}
       {structure === 'domitory' && floor === 1 && <img src={domitory1} alt={'domitory1'} />}
@@ -89,14 +98,13 @@ function Map({ structure, floor, setStructure, setFloor }) {
   </div>;
 }
 
-
-function Teach() {
+function Teach() { //선생님 페이지
   return <div className="teach">
     teach
   </div>;
 }
 
-function Login() {
+function Login() { //로그인 화면 페이지
   return <div className="login">
     login
   </div>;
