a5fb452a9a5ee55b8eb115849f1a1e7ecbfc9c90
kkh061101 <kkh061101@naver.com>
2023-04-12 23:16:07 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -131,8 +131,14 @@ function Login() { //로그인 화면 페이지
 }
 
 function Welcome() {
+  const time = new Date();
   return <div className="welcome">
     welcome
+    <button onClick={e => {
+      localStorage.setItem('logininfo', 'Guest');
+      localStorage.setItem('logintime', time.toLocaleTimeString());
+      window.location.href = '/map'
+    }}>게스트 로그인</button>
     <button onClick={e => window.location.href = '/login'}>로그인 하러가기</button>
   </div>;
 }
