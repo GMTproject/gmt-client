d7442df6f5d2dfac2336d9aa22a7d11e497a4510
kkh061101 <kkh061101@naver.com>
2023-04-25 23:33:02 +0900

    지도 내 이동 버튼 추가

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 import '../styles/Map.scss';
 import center1 from '../imgs/본관1층.png';
 import goldencrown1 from '../imgs/금봉관1층.png';
 import domitory1 from '../imgs/기숙사1층.png';
+import search from '../imgs/search.png';
 
 export default function Map({ structure, floor, setStructure, setFloor }) {
   const [info, setInfo] = useState('');
   return <div className="map">
     <div className="main">
       <div className="sideleft">
-        <input onChange={e => setInfo(e.target.value)} value={info} placeholder='찾고 싶은 실을 검색해 보세요.' />
+        <div className="head">
+          <input onChange={e => setInfo(e.target.value)} value={info} placeholder='    찾고 싶은 실을 검색해 보세요.' />
+          <button><img src={search} alt='search' /></button>
+        </div>
+        <hr />
       </div>
       <div className="img">
-        {/* 지도를 보여주는 부분. 앞 조건이 모두 만족을 하게 되면 뒤에 있는 이미지 태그가 보이게 함 */}
+        <div className="move">
+          <div className="floor">
+            1F
+          </div>
+          <div className="hr"></div>
+          <button>본관</button>
+          <button>금봉관</button>
+          <button>기숙사</button>
+        </div>
         {structure === 'center' && floor === 1 && <img src={center1} alt={'center1'} />}
         {structure === 'goldencrown' && floor === 1 && <img src={goldencrown1} alt={'goldencrown1'} />}
         {structure === 'domitory' && floor === 1 && <img src={domitory1} alt={'domitory1'} />}
