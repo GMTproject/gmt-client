fd755c5fbe8dc5aa7eab84a759f1253c53c66ec5
kkh061101 <kkh061101@naver.com>
2023-07-05 19:57:23 +0900

    There's no time to add

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,7 +3,6 @@ import React from "react";
 import 'styles/Login.scss';
 
 const Login = () => { //로그인 화면 페이지
-  const time = new Date();
   return <div className="login">
     <GauthLoginButton />
   </div>;
