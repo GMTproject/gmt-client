cf0274120a807eb8117e13509f9648edcee4597e
kkh061101 <kkh061101@naver.com>
2023-08-03 23:14:48 +0900

    예외 처리 추가

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -44,13 +44,15 @@ const Map = ({ structure, floor, setFloor, setCenter, setDomitory, setGoldencrow
     }
   }
   async function pushes() {
-    await axios.get(`${url}/teachers`,
-      { headers: { 'Authorization': `Bearer ${JSON.parse(localStorage?.getItem("Tokens"))?.accessToken}` } }).then(e => {
-        console.log(e)
-        setInfos(e.data);
-      }).catch(e =>
-        console.log(e, axios.defaults.headers.common)
-      );
+    if (JSON.parse(localStorage?.getItem("Tokens"))?.accessToken) {
+      await axios.get(`${url}/teachers`,
+        { headers: { 'Authorization': `Bearer ${JSON.parse(localStorage?.getItem("Tokens"))?.accessToken}` } }).then(e => {
+          console.log(e)
+          setInfos(e.data);
+        }).catch(e =>
+          console.log(e, axios.defaults.headers.common)
+        );
+    }
   }
   const initsetting = () => {
     switch (localStorage.getItem('structure')) {
