2cb27c7121f898c4d5e6da402db402123cc6a29f
kkh061101 <kkh061101@naver.com>
2023-08-29 22:22:01 +0900

    /teachers 검색 기능 활성화

--- src/components/Teach.jsx
+++ src/components/Teach.jsx
@@ -54,7 +54,7 @@ const Teach = () => { //선생님 페이지
       calt /= 60000;
       if (calt >= 0) {
         await axios.get(`${url}/teachers/${toggle ? `filter?free=${query.free}&grade1=${query.grade1}
-      &grade2=${query.grade2}&grade3=${query.grade3}&major=${query.major}&skill=${query.skill}` : ''}`).then(e => {
+      &grade2=${query.grade2}&grade3=${query.grade3}&major=${query.major}&skill=${query.skill}` : '?name='}`).then(e => {
           const size = document.body.clientWidth > 520 ? 9 : 4;
           let datas = e.data.reverse();
           datas = sort(datas);
@@ -85,8 +85,7 @@ const Teach = () => { //선생님 페이지
   async function searchingFilter(text) {
     await axios.get(`${url}/teachers?name=${text}`)
       .then(e => {
-        console.log(e.data);
-
+        setInfos(e.data);
       }).catch(err => {
         console.log(err);
       })
